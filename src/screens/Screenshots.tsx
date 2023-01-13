f975450c99df6769111e0a3dfce98a7f95f66a35
antoni.wrobel <antoni.wrobel@imaginecurve.com>
2023-01-14 00:15:16 +0100

    fix

--- src/screens/Screenshots.tsx
+++ src/screens/Screenshots.tsx
@@ -9,19 +9,23 @@ import { getStorage, ref, listAll, getDownloadURL } from 'firebase/storage';
 
 import PQueue from 'p-queue';
 
+const a = {} as any;
+
 const Screenshots = () => {
   const storage = getStorage();
+  const screenshotsRef = ref(storage, 'screenshots');
   const screenshotsRef1 = ref(storage, 'screenshots/magazyn');
   const screenshotsRef2 = ref(storage, 'screenshots/rozliczenia');
   const screenshotsRef3 = ref(storage, 'screenshots/skarbonka');
   const screenshotsRef4 = ref(storage, 'screenshots/wydatki');
 
-  const [items, setItems] = useState({ magazyn: [], rozliczenia: [], skarbonka: [], wydatki: [] }) as any;
+  // const [items, setItems] = useState({ magazyn: [], rozliczenia: [], skarbonka: [], wydatki: [] }) as any;
+  const [items, setItems] = useState([]) as any;
   const [loading, setLoading] = useState(false);
 
   const queue = new PQueue({ concurrency: 1 });
 
-  const getData = () => {
+  const getData = async () => {
     setLoading(true);
 
     const promiseArray = [
@@ -31,41 +35,42 @@ const Screenshots = () => {
       () => listAll(screenshotsRef4)
     ];
 
-    const response = queue.addAll(promiseArray);
-    const temp = {} as any;
+    const response = await queue.addAll(promiseArray);
 
-    response.then(async (res) => {
-      const items = {
-        magazyn: [],
-        rozliczenia: [],
-        skarbonka: [],
-        wydatki: []
-      } as any;
+    const items = {
+      magazyn: [],
+      rozliczenia: [],
+      skarbonka: [],
+      wydatki: []
+    } as any;
 
-      const r = await Promise.all(
-        res.map((folder, index) => {
-          const variant = index === 0 ? 'magazyn' : index === 1 ? 'rozliczenia' : index === 2 ? 'skarbonka' : 'wydatki';
+    const folder = response.map(async (folder, index) => {
+      const urlPromiseArray = folder.items.map((item) => {
+        return () => getDownloadURL(item);
+      });
 
-          const links = folder.items.map((item) => {
-            const url = getDownloadURL(item);
-            return url;
-          });
-
-          return {
-            [variant]: Promise.all(links)
-          };
-        })
-      );
-
-      console.log(r);
+      const result = await queue.addAll(urlPromiseArray);
+      return result.reverse();
     });
 
-    // console.log({ data });
+    const folderPromise = await Promise.all(folder);
+
+    return folderPromise;
   };
 
   useEffect(() => {
-    getData();
-  }, []);
+    (async () => {
+      await getData().then(async (data) => {
+        if (!Object.keys(data).length) {
+          return;
+        }
+
+        setItems(data);
+
+        setLoading(false);
+      });
+    })();
+  }, [setItems, setLoading]);
 
   if (loading) {
     return <div>loading...</div>;
@@ -75,36 +80,35 @@ const Screenshots = () => {
     <Container sx={{ p: '0px !important', m: '24px', maxWidth: '100% !important', width: 'auto' }}>
       <Center>
         {!loading &&
-          Object.keys(items).map((tabKey) => {
-            const item = items[tabKey];
+          //@ts-ignore
+          items.map((folder, index) => {
+            if (!items[index].length) {
+              return;
+            }
 
-            return item.length ? (
-              <Box sx={{ width: '100%' }} key={tabKey}>
-                {item.map((url: any, id: any) => {
-                  if (typeof url !== 'string') {
-                    return;
-                  }
-
+            const variant =
+              index === 0 ? 'magazyn' : index === 1 ? 'rozliczenia' : index === 2 ? 'skarbonka' : 'wydatki';
+            return (
+              <Box key={index} sx={{ width: '100%' }}>
+                <h2>{variant}</h2>
+                {folder.map((url: string) => {
                   return (
-                    <Box key={tabKey + id}>
-                      <Box
-                        sx={{
-                          display: 'flex',
-                          width: '100%',
-                          alignItems: 'center',
-                          justifyContent: 'center',
-                          background: '#dedede',
-                          p: '20px 20px 40px 20px',
-                          boxSizing: 'border-box'
-                        }}
-                      ></Box>
+                    <Box
+                      key={variant + url}
+                      sx={{
+                        display: 'flex',
+                        width: '100%',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        background: '#dedede',
+                        p: '20px 20px 40px 20px',
+                        boxSizing: 'border-box'
+                      }}
+                    >
+                      <img src={url} alt="screen" />
                     </Box>
                   );
                 })}
-              </Box>
-            ) : (
-              <Box sx={{ my: '40px' }} key={tabKey}>
-                Brak danych
               </Box>
             );
           })}
