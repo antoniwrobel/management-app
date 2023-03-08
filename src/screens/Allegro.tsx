4abb47e839b2aa5afff4abed542d39f70ea39bd5
antoni.wrobel <antoni.wrobel@imaginecurve.com>
2023-03-09 00:16:29 +0100

    test

--- src/screens/Allegro.tsx
+++ src/screens/Allegro.tsx
@@ -16,7 +16,7 @@ export const Allegro = () => {
 
   const handleUserAuth = async (code: string) => {
     try {
-      const url = `https://cors-anywhere.herokuapp.com/https://allegro.pl/auth/oauth/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect_uri}`;
+      const url = `https://allegro.pl/auth/oauth/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect_uri}`;
       const client_id = process.env.REACT_APP_CLIENT_ID;
       const client_secret = process.env.REACT_APP_CLIENT_SECRET;
       const encodedString = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
