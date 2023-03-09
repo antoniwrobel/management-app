d2a571bcc763e1567882cf41ad615c993f8f4948
antoni.wrobel <antoni.wrobel@imaginecurve.com>
2023-03-09 14:27:59 +0100

    test

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,6 +4,6 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from '@firebase/firestore';
 
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 export const auth = getAuth();
 export const Providers = { google: new GoogleAuthProvider() };
-export const db = getFirestore(app);
